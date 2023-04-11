05ae590ad9bfa6cc8a6c8abb08cfe99f4f640d8b
sumurillo <sumire.ohana@gmail.com>
2023-04-11 10:49:49 -0500

    change database

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,7 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn');
 // All paths start with '/api/users'
 
 // POST /api/users (create a user - sign up)
-router.post('/signup', usersCtrl.create);
+router.post('/', usersCtrl.create);
 // POST /api/users/login
 router.post('/login', usersCtrl.login);
 // GET /api/users/check-token
